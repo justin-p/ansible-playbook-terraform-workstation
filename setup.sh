fe86b1d2ce8e1c9f5ccb2100998862b59d601a43
Justin Perdok <justin-p@users.noreply.github.com>
2020-10-07 14:44:53 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,14 +5,14 @@ echo ''
 sudo apt-get update -y >/dev/null 2>&1
 sudo apt-get install git curl python3 python3-pip -y >/dev/null 2>&1
 pip3 install ansible > /dev/null 2>&1
-git clone https://github.com/justin-p/ansible-terraform-workstation /tmp/repo_folder >/dev/null 2>&1
+git clone https://github.com/justin-p/ansible-playbook-terraform-workstation /tmp/ansible_playbook_terraform_workstation >/dev/null 2>&1
 export PATH=$PATH:/$HOME/.local/bin
 
 echo 'PREPARE [Installing requirements with Ansible-Galaxy.] ********'
 echo ''
-ansible-galaxy install -r /tmp/repo_folder/files/requirements.yml >/dev/null 2>&1
+ansible-galaxy install -r /tmp/ansible_playbook_terraform_workstation/files/requirements.yml >/dev/null 2>&1
 echo 'RUN [Running playbook.] ***************************************'
-ansible-playbook /tmp/repo_folder/main.yml -i /tmp/repo_folder/inventory.yml
+ansible-playbook /tmp/ansible_playbook_terraform_workstation/main.yml -i /tmp/ansible_playbook_terraform_workstation/inventory.yml
 
 echo -e '\e[35m[WARNING]: Please reboot the system to apply all changes.'
 echo ''
